081b948ad33d42b73da34bbf1e3306a09fb86bb9
marovichn <nikolamarovic.11@gmail.com>
2023-07-22 14:59:09 +0200

    Components-creation

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,20 @@
 import { Inter } from "next/font/google";
+import { Product, FooterBanner, HeroBanner } from "@/components";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <span className="flex justify-between w-full -mt-10">NAV <span>user | cart</span></span>
-      <hr></hr>
-      Banner
-      <hr></hr>
-      products
-      <hr></hr>
-      footer Banner
-    </main>
+    <>
+      hero Banner
+      <div className="products-heading">
+        <h2>Best Selling Products</h2>
+        <p>Speakers...</p>
+      </div>
+
+      <div>
+        {["prodduct1", "product2"].map(product=><div>{product}</div>)}
+      </div>
+    </>
   );
 }
