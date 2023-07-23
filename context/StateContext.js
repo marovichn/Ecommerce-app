6deadbe0a965fc0fd2da8b89377b7052d191a258
marovichn <nikolamarovic.11@gmail.com>
2023-07-23 02:45:17 +0200

    Product-Details-context

--- context/StateContext.js
+++ context/StateContext.js
@@ -5,28 +5,39 @@ const Context = createContext();
 
 export const StateContext = ({ children }) => {
   const [showCart, setShowCart] = useState(false);
-  const [cartItems, setCartItems] = useState();
-  const [totalPrice, setTotalPrice] = useState();
-  const [totalQuantities, setTotalQuantities] = useState();
+  const [cartItems, setCartItems] = useState([]);
+  const [totalPrice, setTotalPrice] = useState(0);
+  const [totalQuantities, setTotalQuantities] = useState(0);
   const [qty, setQty] = useState(1);
 
   const incQty = () => {
-    setQty((prev) => prev + 1);
+    setQty((prevQty) => prevQty + 1);
   };
+
   const decQty = () => {
-    setQty((prev) => {
-      if (prev < 1) {
-        return 1;
-      }
-      return prev - 1;
+    setQty((prevQty) => {
+      if (prevQty - 1 < 1) return 1;
+
+      return prevQty - 1;
     });
   };
 
   return (
     <Context.Provider
-      value={{ showCart, cartItems, totalPrice, totalQuantities, qty, incQty, decQty }}
+      value={{
+        showCart,
+        setShowCart,
+        cartItems,
+        totalPrice,
+        totalQuantities,
+        qty,
+        incQty,
+        decQty
+      }}
     >
       {children}
     </Context.Provider>
   );
 };
+
+export const useStateContext = () => useContext(Context);
